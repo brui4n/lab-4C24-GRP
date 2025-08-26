39f871bf2559ca1d4fb9821373544c2c817e7d6b
milennne <milene.fuentes@tecsup.edu.pe>
2025-08-25 21:58:21 -0500

    Modificación de Milene Fuentes

---
+++ src/StudentReport.java
@@ -0,0 +1,7 @@
+public class StudentController {
+    public void controller () {
+        System.out.println("Hola mundo");
+        System.out.println("Milene Fuentes");
+        System.out.println("Modifiqué report");
+    }
+}
