f485e27bc3428eb83a2fb8597a182d2a24950fbc
brui4n <coronelbryan900@gmail.com>
2025-08-25 21:47:15 -0500

    Modificacion de clase (Bryan Coronel)

--- src/StudentController.java
+++ src/StudentController.java
@@ -2,5 +2,6 @@ public class StudentController {
     public void controller () {
         System.out.println("Hola mundo");
         System.out.println("Bryan Coronel");
+        System.out.println("Modificacion");
     }
 }
